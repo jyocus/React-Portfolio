29aa2e52b190b63422121f4d488df0cd5bc56ab0
Justin Yocus <justin.o.yocus@gmail.com>
2020-11-24 19:03:30 -0600

    added file/page structure, basic routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Portfolio from "./pages/Portfolio/index.js";
+import Contact from "./pages/Contact/index.js";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Wrapper>
+          <Route exact path="/" component={Portfolio} />
+          <Route exact path="/about" component={Portfolio} />
+          <Route exact path="/discover" component={Contact} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
